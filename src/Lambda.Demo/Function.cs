410d495949be2e0b5200ddab187a1e2b040a9a4d
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-11-25 20:26:52 -0500

    DOC:  Deployment result.

--- src/Lambda.Demo/Function.cs
+++ src/Lambda.Demo/Function.cs
@@ -40,3 +40,31 @@ namespace Lambda.Demo
         }
     }
 }
+
+/*
+ 
+ Deployment to AWS Reults
+--------------------------
+PS D:\repo\Lambda.Demo\src\Lambda.Demo> dotnet lambda deploy-function LambdaDemo
+Amazon Lambda Tools for .NET Core applications (5.6.2)
+Project Home: https://github.com/aws/aws-extensions-for-dotnet-cli, https://github.com/aws/aws-lambda-dotnet
+
+Executing publish command
+... invoking 'dotnet publish', working folder 'D:\repo\Lambda.Demo\src\Lambda.Demo\bin\Release\netcoreapp3.1\publish'
+... dotnet publish "D:\repo\Lambda.Demo\src\Lambda.Demo" --output "D:\repo\Lambda.Demo\src\Lambda.Demo\bin\Release\netcoreapp3.1\publish" --configuration "Release" --framework "netcoreapp3.1" /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64 --self-contained False
+... publish: MSBuild version 17.3.2+561848881 for .NET
+... publish:   Determining projects to restore...
+... publish:   All projects are up-to-date for restore.
+... publish:   Lambda.Demo -> D:\repo\Lambda.Demo\src\Lambda.Demo\bin\Release\netcoreapp3.1\linux-x64\Lambda.Demo.dll
+... publish:   Lambda.Demo -> D:\repo\Lambda.Demo\src\Lambda.Demo\bin\Release\netcoreapp3.1\publish\
+Zipping publish folder D:\repo\Lambda.Demo\src\Lambda.Demo\bin\Release\netcoreapp3.1\publish to D:\repo\Lambda.Demo\src\Lambda.Demo\bin\Release\netcoreapp3.1\Lambda.Demo.zip
+... zipping: Amazon.Lambda.Core.dll
+... zipping: Amazon.Lambda.Serialization.SystemTextJson.dll
+... zipping: Lambda.Demo.deps.json
+... zipping: Lambda.Demo.dll
+... zipping: Lambda.Demo.pdb
+... zipping: Lambda.Demo.runtimeconfig.json
+Created publish archive (D:\repo\Lambda.Demo\src\Lambda.Demo\bin\Release\netcoreapp3.1\Lambda.Demo.zip).
+Updating code for existing function LambdaDemo
+
+ */
